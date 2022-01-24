24161880fc317018915cbbdd0e91aaf4881f545f
Colin Bob <colin@sweepsouth.com>
2022-01-24 09:43:03 +0200

    change mid to middle

--- python/binary_search.py
+++ python/binary_search.py
@@ -3,15 +3,15 @@ def binary_search(list, item):
   high = len(list) - 1
 
   while low <= high:
-    mid = (low + high) / 2
-    guess = list[mid]
+    middle = (low + high) / 2
+    guess = list[middle]
 
     if guess == item:
-      return mid
+      return middle
     if guess > item:
-      high = mid - 1
+      high = middle - 1
     else:
-      low = mid + 1
+      low = middle + 1
     
   return None
 
